ff7a493397382c477e63574a3ef569f207daa564
Matheus Ferraz Oliveira <matheus.foliveira@sptech.school>
2022-05-19 22:16:45 -0300

    Alteração na organização das pastas. Alguns avanços na Tela de Cadastro

--- JS/indieland_main.js
+++ JS/indieland_main.js
@@ -1,5 +1,9 @@
+const TimeOut = setTimeout(chamar_cadastro, 1000);
+
 function chamar_cadastro() {
     document.getElementById('card_cadastro').style.display = 'flex'
+    document.getElementById('card_cadastro').style.opacity = 1
+
     document.getElementById('card_inicial').style.display = 'none'
 }
 
